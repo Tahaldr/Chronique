95dfa59763faded15a5954f8f50ae3cb8cc4017b
Tahaldr <tahanaitdo@gmail.com>
2024-10-31 11:33:46 +0100

    cloudinary And Redis config added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,31 +1,34 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.schem
+const userSchema = new mongoose.schem();
 
 const mongoose = require("mongoose");
 const Schema = mongoose.schema({
-  userPic : {
-    type : String,
-    default : 'https://res.cloudinary.com/dv48ogvly/image/upload/v1730020149/Default_pfp_y29ox0.jpg',
-    
+  userPic: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/dv48ogvly/image/upload/v1730020149/Default_pfp_y29ox0.jpg",
   },
-  name : {
-    type : String,
-    required : true,
-    unique: true
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    unique: [true, "Name already exists"],
   },
   email: {
-    type : String,
-    required : true,
-    unique: true
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email already exists"],
   },
-  password :{
-    type : String,
-    required : true
+  password: {
+    type: String,
+    required: [true, "Password is required"],
   },
   idAdmin: {
-    type : Boolean,
-    default : false
-  }
-})
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
