d8b2dfb9ab2b48083291bab2db082e69f2a28c9c
Tahaldr <tahanaitdo@gmail.com>
2025-03-16 12:23:05 +0000

    Finished : - Add a characters counter like '12/500' in post content form

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const postSchema = new mongoose.Schema(
   {
@@ -8,48 +8,51 @@ const postSchema = new mongoose.Schema(
       validate: {
         validator: function (value) {
           // Allow null, undefined, or an empty string to bypass the format check
-          return !value || value === "null" || /\.(png|jpe?g)$/i.test(value);
+          return !value || value === 'null' || /\.(png|jpe?g)$/i.test(value);
         },
-        message: "Only PNG and JPG images are allowed",
+        message: 'Only PNG and JPG images are allowed',
       },
       default: null, // Ensure the default is null if no image is provided
     },
 
     title: {
       type: String,
-      required: [true, "Title is required"],
+      required: [true, 'Title is required'],
+      unique: [true, 'Title already exists'],
+      maxlength: [130, 'Title must be less than 130 characters'],
     },
     author: {
       // reference to user model
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: [true, "Author is required"],
+      ref: 'User',
+      required: [true, 'Author is required'],
     },
     description: {
       type: String,
-      required: [true, "Description is required"],
-      maxlength: [500, "Description must be less than 500 characters"],
+      required: [true, 'Description is required'],
+      maxlength: [500, 'Description must be less than 500 characters'],
     },
     content: {
       type: String,
-      required: [true, "Content is required"],
+      required: [true, 'Content is required'],
+      maxlength: [10000, 'Content must be less than 10000 characters'],
     },
     category: {
       type: String,
-      required: [true, "Category is required"],
+      required: [true, 'Category is required'],
     },
     tags: {
       type: [String],
     },
     likes: {
       type: [mongoose.Schema.Types.ObjectId],
-      ref: "User",
+      ref: 'User',
       default: [],
     },
   },
   { timestamps: true }
 );
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 export default Post;
