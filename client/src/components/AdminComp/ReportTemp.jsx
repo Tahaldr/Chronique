7e0085233462e778c0e34da8bb6f021b162ce265
TahaAlt <tahanaiteddouch@gmail.com>
2025-03-21 14:44:43 +0000

    Checkpoint while making the report pdf template

--- client/src/components/AdminComp/ReportTemp.jsx
+++ client/src/components/AdminComp/ReportTemp.jsx
@@ -1,18 +1,11 @@
 import { useContext } from "react";
 import { AdminDashboardContext } from "../../App";
-import {
-  Document,
-  Page,
-  Text,
-  View,
-  StyleSheet,
-  Font,
-} from "@react-pdf/renderer";
+import { Document, Page, Text, View, Font, Image } from "@react-pdf/renderer";
 
 // Register custom font
 Font.register({
   family: "smallFont",
-  src: "/FontFaces/EbGaramond/EBGaramond-Medium.ttf", // Path is relative to the public folder
+  src: "/FontFaces/EbGaramond/EBGaramond-Regular.ttf", // Path is relative to the public folder
 });
 
 Font.register({
@@ -25,52 +18,75 @@ Font.register({
   src: "/FontFaces/cheltenham/cheltenham-cond-normal-700.ttf",
 });
 
-const styles = StyleSheet.create({
-  page: { padding: 20, fontSize: 12 },
-  section: { marginBottom: 10 },
-  title: {
-    fontSize: 20,
-    marginBottom: 10,
-    fontFamily: "bigFont",
-  },
-  subtitle: {
-    marginBottom: 10,
-    fontFamily: "mediumFont",
-  },
-  text: { marginBottom: 5, fontFamily: "smallFont" },
-  boldText: { fontFamily: "mediumFont" },
-});
-
 const ReportTemp = ({ reportSelected }) => {
   return (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <Text style={styles.title}>{reportSelected?.type} Report</Text>
-        <View style={styles.section}>
-          <Text style={styles.subtitle}>Report Id: {reportSelected?._id}</Text>
-          <Text style={styles.subtitle}>
-            Report Post: {reportSelected?.post}
-          </Text>
-          {reportSelected?.comment && (
+    <>
+      {/* <Document>
+        <Page
+          size="A4"
+          style={{
+            backgroundColor: "red",
+          }}
+        >
+          <Text style={styles.title}>{reportSelected?.type} Report</Text>
+          <View style={styles.section}>
             <Text style={styles.subtitle}>
-              Report Comment: {reportSelected?.comment}
+              Report Id: {reportSelected?._id}
             </Text>
-          )}
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.boldText}>Reporter ID:</Text>
-          <Text style={styles.text}>{reportSelected?.reporter?._id}</Text>
-          <Text style={styles.boldText}>Reporter Name:</Text>
-          <Text style={styles.text}>{reportSelected?.reporter?.name}</Text>
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.boldText}>Reason:</Text>
-          <Text style={styles.text}>{reportSelected?.reason}</Text>
-          <Text style={styles.boldText}>Content:</Text>
-          <Text style={styles.text}>{reportSelected?.description}</Text>
-        </View>
-      </Page>
-    </Document>
+            <Text style={styles.subtitle}>
+              Report Post: {reportSelected?.post}
+            </Text>
+            {reportSelected?.comment && (
+              <Text style={styles.subtitle}>
+                Report Comment: {reportSelected?.comment}
+              </Text>
+            )}
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.text}>
+              <Text style={styles.boldText}>Reporter Id:</Text>{" "}
+              {reportSelected?.reporter?._id}
+            </Text>
+            <Text style={styles.text}>
+              <Text style={styles.boldText}>Reporter Name:</Text>{" "}
+              {reportSelected?.reporter?.name}
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.boldText}>Reason:</Text>
+            <Text style={styles.text}>{reportSelected?.reason}</Text>
+            <Text style={styles.boldText}>Content:</Text>
+            <Text style={styles.text}>{reportSelected?.description}</Text>
+          </View>
+        </Page>
+      </Document> */}
+
+      <Document>
+        <Page
+          size="A4"
+          style={{
+            paddingHorizontal: 30,
+            paddingVertical: 20,
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              justifyContent: "center",
+            }}
+          >
+            <Image
+              src="/Logo/ChroniqueBlack2.png"
+              style={{ width: 40, height: 40 }}
+            />
+          </View>
+          <View>
+            
+          </View>
+        </Page>
+      </Document>
+    </>
   );
 };
 
