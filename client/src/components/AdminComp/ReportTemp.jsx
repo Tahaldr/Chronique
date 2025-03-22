ec58dc6eddea90ff1e3e8a7a87a62bb40bc98cb7
Tahaldr <tahanaitdo@gmail.com>
2025-03-22 00:58:36 +0000

    Finished downloading report as PDF design and functionality

--- client/src/components/AdminComp/ReportTemp.jsx
+++ client/src/components/AdminComp/ReportTemp.jsx
@@ -1,93 +1,156 @@
-import { useContext } from "react";
-import { AdminDashboardContext } from "../../App";
-import { Document, Page, Text, View, Font, Image } from "@react-pdf/renderer";
+import { Document, Page, Text, View, Font, Image } from '@react-pdf/renderer';
+import PropTypes from 'prop-types';
+import moment from 'moment';
 
 // Register custom font
 Font.register({
-  family: "smallFont",
-  src: "/FontFaces/EbGaramond/EBGaramond-Regular.ttf", // Path is relative to the public folder
+  family: 'smallFont',
+  src: '/FontFaces/EbGaramond/EBGaramond-Regular.ttf', // Path is relative to the public folder
 });
 
 Font.register({
-  family: "mediumFont",
-  src: "/FontFaces/EbGaramond/EBGaramond-SemiBold.ttf",
+  family: 'mediumFont',
+  src: '/FontFaces/EbGaramond/EBGaramond-SemiBold.ttf',
 });
 
 Font.register({
-  family: "bigFont",
-  src: "/FontFaces/cheltenham/cheltenham-cond-normal-700.ttf",
+  family: 'bigFont',
+  src: '/FontFaces/cheltenham/cheltenham-cond-normal-700.ttf',
 });
 
 const ReportTemp = ({ reportSelected }) => {
   return (
     <>
-      {/* <Document>
+      <Document>
         <Page
-          size="A4"
+          size='A4'
           style={{
-            backgroundColor: "red",
-          }}
-        >
-          <Text style={styles.title}>{reportSelected?.type} Report</Text>
-          <View style={styles.section}>
-            <Text style={styles.subtitle}>
-              Report Id: {reportSelected?._id}
+            position: 'relative',
+            paddingHorizontal: 30,
+            paddingVertical: 20,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 20,
+          }}>
+          {/* Chronique logo */}
+          <View
+            style={{
+              display: 'flex',
+              alignItems: 'flex-end',
+              justifyContent: 'center',
+            }}>
+            <Image src='/Logo/ChroniqueBlack2.png' style={{ width: 35, height: 35 }} />
+          </View>
+
+          {/* Page title */}
+          <View>
+            <Text style={{ fontSize: 24, fontFamily: 'bigFont', textTransform: 'capitalize' }}>
+              {reportSelected?.type} Report
             </Text>
-            <Text style={styles.subtitle}>
-              Report Post: {reportSelected?.post}
+          </View>
+
+          {/* report ref */}
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 10,
+              fontFamily: 'smallFont',
+              fontSize: 12,
+              width: '40%',
+            }}>
+            <Text
+              style={{
+                backgroundColor: 'rgba(193, 193, 193, 0.5)',
+                paddingHorizontal: 10,
+              }}>
+              report id : {reportSelected?._id}
+            </Text>
+            <Text
+              style={{
+                backgroundColor: 'rgba(193, 193, 193, 0.5)',
+                paddingHorizontal: 10,
+              }}>
+              report post : {reportSelected?.post}
             </Text>
             {reportSelected?.comment && (
-              <Text style={styles.subtitle}>
-                Report Comment: {reportSelected?.comment}
+              <Text
+                style={{
+                  backgroundColor: '#c1c1c1',
+                  paddingHorizontal: 10,
+                }}>
+                report comment : {reportSelected?.comment}
               </Text>
             )}
           </View>
-          <View style={styles.section}>
-            <Text style={styles.text}>
-              <Text style={styles.boldText}>Reporter Id:</Text>{" "}
+
+          {/* reporter info */}
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 5,
+              borderLeftWidth: 1,
+              borderLeftColor: '#c1c1c1',
+              borderLeftStyle: 'solid',
+              paddingLeft: 12,
+              fontFamily: 'smallFont',
+              fontSize: 12,
+            }}>
+            <Text>
+              <Text style={{ fontFamily: 'mediumFont' }}>Reporter id :</Text>{' '}
               {reportSelected?.reporter?._id}
             </Text>
-            <Text style={styles.text}>
-              <Text style={styles.boldText}>Reporter Name:</Text>{" "}
+            <Text>
+              <Text style={{ fontFamily: 'mediumFont' }}>Reporter name :</Text>{' '}
               {reportSelected?.reporter?.name}
             </Text>
           </View>
-          <View style={styles.section}>
-            <Text style={styles.boldText}>Reason:</Text>
-            <Text style={styles.text}>{reportSelected?.reason}</Text>
-            <Text style={styles.boldText}>Content:</Text>
-            <Text style={styles.text}>{reportSelected?.description}</Text>
-          </View>
-        </Page>
-      </Document> */}
 
-      <Document>
-        <Page
-          size="A4"
-          style={{
-            paddingHorizontal: 30,
-            paddingVertical: 20,
-          }}
-        >
+          {/* Report details */}
           <View
             style={{
-              display: "flex",
-              alignItems: "flex-end",
-              justifyContent: "center",
-            }}
-          >
-            <Image
-              src="/Logo/ChroniqueBlack2.png"
-              style={{ width: 40, height: 40 }}
-            />
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 5,
+              borderLeftWidth: 1,
+              borderLeftColor: '#c1c1c1',
+              borderLeftStyle: 'solid',
+              paddingLeft: 12,
+              fontFamily: 'smallFont',
+              fontSize: 12,
+            }}>
+            <Text>
+              <Text style={{ fontFamily: 'mediumFont' }}>Reason :</Text> {reportSelected?.reason}
+            </Text>
+            <Text>
+              <Text style={{ fontFamily: 'mediumFont' }}>Content :</Text>{' '}
+              {reportSelected?.description}
+            </Text>
           </View>
-          <View>
-            
+
+          {/* Report footer */}
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 20,
+              right: 30,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 5,
+              fontFamily: 'smallFont',
+              fontSize: 12,
+            }}>
+            <Text>Chronique - {moment().format('DD/MM/YYYY')}</Text>
           </View>
         </Page>
       </Document>
     </>
   );
+};
+
+ReportTemp.propTypes = {
+  reportSelected: PropTypes.object.isRequired,
 };
 
 export default ReportTemp;
